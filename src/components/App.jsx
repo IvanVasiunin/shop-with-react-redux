7da7a6ef453b4b62182a3744204bbf640323998f
Ivan <ivanvasyunin@gmail.com>
2023-12-25 18:26:16 +0100

    async code and data bse requests added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,49 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './Cart/Cart';
 import Layout from './Layout/Layout';
 import Products from './Shop/Products';
+import Notification from './UI/Notification';
+import { useEffect, Fragment } from 'react';
+import { sendCartData, fetchCartData } from 'store/cart-actions';
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
