6acd006f5479de706d164dbb8dd552020f87da6f
Ivan <ivanvasyunin@gmail.com>
2023-12-25 09:58:11 +0100

    project structure reworked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
+
 import Cart from './Cart/Cart';
 import Layout from './Layout/Layout';
 import Products from './Shop/Products';
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
